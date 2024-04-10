e8fa4b08db865d0a6bfaa31f024f4449ef85e67a
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-10 17:14:14 +0100

    add graph method to clear all points and segments, remove functions and buttons from html

--- js/math/graph.js
+++ js/math/graph.js
@@ -1,49 +1,65 @@
-class Graph{
-    constructor(points=[],segments=[]){
-        this.points = points
-        this.segments = segments
-    }
-    addPoint(point){
-        this.points.push(point)
-    }
-    containsPoint(point){
-        return this.points.find((p)=> p.equals(point))
-    }
+class Graph {
+  constructor(points = [], segments = []) {
+    this.points = points;
+    this.segments = segments;
+  }
+  addPoint(point) {
+    this.points.push(point);
+  }
+  containsPoint(point) {
+    return this.points.find((p) => p.equals(point));
+  }
+  dispose() {
+    this.points.length = 0;
+    this.segments.length = 0;
+  }
 
-    draw(ctx){
-        for(const seg of this.segments){
-            seg.draw(ctx)
-        }
-        for (const point of this.points){
-            point.draw(ctx)
-        }
+  draw(ctx) {
+    for (const seg of this.segments) {
+      seg.draw(ctx);
     }
-    tryAddPoint(point){
-        if (!this.containsPoint(point)){
-            this.addPoint(point)
-            return true
-        }
-        return false
+    for (const point of this.points) {
+      point.draw(ctx);
     }
+  }
+  tryAddPoint(point) {
+    if (!this.containsPoint(point)) {
+      this.addPoint(point);
+      return true;
+    }
+    return false;
+  }
 
-    addSegment(seg){
-        this.segments.push(seg)
+  addSegment(seg) {
+    this.segments.push(seg);
+  }
+  containsSegment(seg) {
+    return this.segments.find((s) => s.equals(seg));
+  }
+  getSegmentsWithPoint(point) {
+    const segs = [];
+    for (const seg of this.segments) {
+      if (seg.includes(point)) {
+        segs.push(seg);
+      }
     }
-    containsSegment(seg){
-        return this.segments.find((s)=> s.equals(seg))
+    return segs;
+  }
+  tryAddSegment(seg) {
+    if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+      this.addSegment(seg);
+      return true;
     }
-    tryAddSegment(seg){
-        if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)){
-            this.addSegment(seg)
-            return true
-        }
-        return false
+    return false;
+  }
+  removeSegment(seg) {
+    this.segments.splice(this.segments.indexOf(seg), 1);
+  }
+  removePoint(point) {
+    const segs = this.getSegmentsWithPoint(point);
+    for (const seg of segs) {
+      this.removeSegment(seg);
     }
-    removeSegment(seg){
-        this.segments.splice(this.segments.indexOf(seg),1)
-    }
-    removePoint(point){
-        this.points.splice(this.points.indexOf(point),1)
-    }
-
+    this.points.splice(this.points.indexOf(point), 1);
+  }
 }
