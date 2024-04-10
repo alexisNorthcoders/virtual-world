0636cb3824c97524aaaa013690c7d0109d4d3573
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-10 17:06:49 +0100

    add functions and methods to add/remove points and segments

--- js/math/graph.js
+++ js/math/graph.js
@@ -29,5 +29,21 @@ class Graph{
     addSegment(seg){
         this.segments.push(seg)
     }
+    containsSegment(seg){
+        return this.segments.find((s)=> s.equals(seg))
+    }
+    tryAddSegment(seg){
+        if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)){
+            this.addSegment(seg)
+            return true
+        }
+        return false
+    }
+    removeSegment(seg){
+        this.segments.splice(this.segments.indexOf(seg),1)
+    }
+    removePoint(point){
+        this.points.splice(this.points.indexOf(point),1)
+    }
 
 }
