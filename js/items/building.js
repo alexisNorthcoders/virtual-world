501a697ea12ffade5e515a2addb2aebb4930306a
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-22 12:12:34 +0100

    add sides to buildings

--- js/items/building.js
+++ js/items/building.js
@@ -7,7 +7,20 @@ class Building{
     draw(ctx,viewPoint){
         const topPoints = this.base.points.map((p)=> add(p,scale(subtract(p,viewPoint), this.heightCoef)))
         const ceiling = new Polygon(topPoints)
+
+        const sides = []
+        for (let i=0;i<this.base.points.length;i++){
+            const nextI = (i+1)% this.base.points.length
+            const poly = new Polygon([
+                this.base.points[i],this.base.points[nextI],
+                topPoints[nextI],topPoints[i]
+            ])
+            sides.push(poly)
+        }
         this.base.draw(ctx,{fill:"white", stroke:"#AAA"})
+        for (const side of sides){
+            side.draw(ctx,{fill:"white", stroke:"#AAA"})
+        }
         ceiling.draw(ctx,{fill:"white", stroke:"#AAA"})
     }
 }
